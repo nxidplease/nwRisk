c651ca1c91b1bcb617efe75a07a3899b83a1089e
Anatoli Krasilshikov <anatoli_70@walla.com>
2017-06-23 10:08:29 +0300

    Temporarily changed gui code, to invoke battle dialog right ahead. Added some styles for button and slider alignment in the battle dialog, started to implement the battle phases idea...

--- battleDialogController.js
+++ battleDialogController.js
@@ -1,38 +1,135 @@
 const MAX_ATTACKERS = 3;
 const MAX_DEFENDERS = 2;
-
+const CONTROL_THUMB_WIDTH = 20;
+const INPUT_RANGE_WIDTH_PERCENT = 75;
+const BATTLE_DLG_STATES = {
+        ATTACKER_PAHSE: 0
+        , DEFENDER_PHASE: 1
+        , RESULT_PHASE: 2
+    }
+    /*
+        This class is in charge of all the logic behind the dialog
+    */
 function BattleDialogController(sourceArea, destArea) {
     this.sourceArea = sourceArea;
     this.destArea = destArea;
-    this.modalBackground = select('.modal-background');
-    this.modalContentDiv = select('.modal-content');
-
-    if (this.modalBackground === null) {
-        this.modalBackground = createDiv('');
-        this.modalBackground.addClass('modal-background');
-        this.modalContentDiv = createDiv('');
-        this.modalContentDiv.addClass('modal-content');
+    // Calculate maximum attackers amount and pass it to the view
+    this.view = new BattleDialogView(Math.min(this.sourceArea.units - 1, MAX_ATTACKERS));
+    this.show = this.view.show.bind(this.view);
+    this.attackerPhase = function () {}
+}
+/*
+    This class is in charge of building the html of the dialog
+*/
+function UnitAmountSelector(maxSliderVal, callback) {
+    let headerBodyBackground = getModalHeaderBodyBgDivs();
+    let updateSlider = function (mavVal) {
+        // If slider was created previously, only update it's maxVal
+        if (UnitAmountSelector.slider) {
+            UnitAmountSelector.slider.attribute('max', maxVal);
+        }
+        else {
+            let slider = createSlider(1, maxSliderVal, maxSliderVal, 1);
+            slider.id('unitSlider');
+            createSliderOut(maxVal);
+            let sliderOut = UnitAmountSelector.sliderOut;
+            sliderOut.value = maxSliderVal;
+            slider.input(recalcSliderOutPos);
+            let recalcSliderOutPos = function () {
+                let maxVal = parseInt(slider.attribute('max'));
+                let minVal = parseInt(slider.attribute('min'));
+                let range = maxVal - minVal;
+                let sliderPecent = ((slider.value() - minVal) / range) * 100;
+                let positionOffset = Math.round(CONTROL_THUMB_WIDTH * sliderPecent / 100) - (CONTROL_THUMB_WIDTH / 2);
+                sliderOut.elt.style.left = 'calc(' + sliderPecent + '% - ' + positionOffset + 'px)';
+            }
+            recalcSliderOutPos();
+            UnitAmountSelector.slider = slider;
+        }
     }
-
-    var maxAttackers = Math.min(this.sourceArea.units - 1, MAX_ATTACKERS);
-    buildHtml(maxAttackers);
-
+    let createSliderOut = function () {
+        if (UnitAmountSelector.sliderOut) {
+            return UnitAmountSelector.sliderOut;
+        }
+        else {
+            let sliderOut = createElement('output');
+            sliderOut.id('sliderOut');
+            sliderOut.attribute('for', 'unitSlider');
+            UnitAmountSelector.sliderOut = sliderOut;
+            return sliderOut;
+        }
+    }
+    let createSliderForm = function () {
+            let form = createElement('form');
+            form.id('sliderForm');
+            form.attribute('oninput', 'sliderOut.value = unitSlider.valueAsNumber');
+            form.addClass('sliderForm');
+            return form;
+        }
+        // Setting slider style
+    var slider = createSlider(1, maxSliderVal, maxSliderVal, 1);
+    slider.id('unitSlider');
+    var header = createElement('h2', 'How many units would you like to use for the attack?');
+    var headerDiv = createDiv('');
+    var bodyDiv = createDiv('');
+    var form = createElement('form');
+    form.id('sliderForm');
+    form.attribute('oninput', 'sliderOut.value = unitSlider.valueAsNumber');
+    form.addClass('sliderForm');
+    var actionBtn = createButton('Attack');
+    actionBtn.addClass('action-button');
+    var sliderOut = createElement('output');
+    sliderOut.id('sliderOut');
+    sliderOut.attribute('for', 'unitSlider');
+    sliderOut.value = maxSliderVal;
+    slider.input(function () {
+        var maxVal = parseInt(slider.attribute('max'));
+        var minVal = parseInt(slider.attribute('min'));
+        var range = maxVal - minVal;
+        var sliderPecent = ((slider.value() - minVal) / range) * 100;
+        var positionOffset = Math.round(CONTROL_THUMB_WIDTH * sliderPecent / 100) - (CONTROL_THUMB_WIDTH / 2);
+        sliderOut.elt.style.left = 'calc(' + sliderPecent + '% - ' + positionOffset + 'px)';
+    });
+    actionBtn.mousePressed(() => {
+        bgAndContent.background.elt.style.display = 'none';
+        callback(slider.value());
+    })
+    headerDiv.addClass('modal-header');
+    headerDiv.child(header);
+    bodyDiv.addClass('modal-body');
+    bodyDiv.child(form);
+    form.child(sliderOut);
+    form.child(slider);
+    bodyDiv.child(actionBtn);
+    bgAndContent.content.child(headerDiv);
+    bgAndContent.content.child(bodyDiv);
     this.show = function () {
-        this.modalBackground.elt.style.display = 'block';
+        bgAndContent.background.elt.style.display = 'block';
     }
+}
 
-    var buildHtml = function (maxSliderVal) {
-        var slider = createSlider(1, maxSliderVal, maxSliderVal, 1);
-        var header = createElement('h2', 'How many units would you like to use?');
-        var headerDiv = createDiv('');
-        var bodyDiv = createDiv('');
+function getModalHeaderBodyBgDivs() {
+    if (getModalHeaderBodyBgDivs.modalDivs) {
+        return getModalHeaderBodyBgDivs.modalDivs;
+    }
+    else {
+        let modalBackground = createDiv('');
+        modalBackground.addClass('modal-background');
+        let modalContentDiv = createDiv('');
+        modalContentDiv.addClass('modal-content');
+        modalBackground.child(this.modalContentDiv);
+        let headerDiv = createDiv('');
+        let bodyDiv = createDiv('');
         headerDiv.addClass('modal-header');
-        headerDiv.child(header);
-
         bodyDiv.addClass('modal-body');
-        bodyDiv.child(slider);
-        this.modalContentDiv.child(headerDiv);
-        this.modalContentDiv.child(bodyDiv);
-
+        modalContentDiv.child(headerDiv);
+        modalContentDiv.child(bodyDiv);
+        let modalDivs = {
+            header: headerDiv
+            , body: bodyDiv
+            , background: modalBackground
+        };
+        getModalHeaderBodyBgDivs.modalDivs = modalDivs;
+        return modalDivs;
     }
 }
--- gui.js
+++ gui.js
@@ -1,19 +1,16 @@
 const GAME_MAP_PATH = 'assets/risk-colored-small.jpg';
 const COLOR_MAP_PATH = 'assets/risk-colored-small-color-map.bmp';
-
 // UI Elements
 var currTurnP;
 var nextPhaseButton;
 var gameMap;
 var colorMap;
 var centerSize = 25;
-
 var currPlayer;
 var watingForGameLogic = true;
 var colorAreaNameMap;
 var highlightedAreaName = '';
 var areas;
-
 // Const key codes
 const KEY_MAP = {
     'SPACE': 32
@@ -23,19 +20,15 @@ const KEY_MAP = {
     , 's': 83
     , 'c': 67
 };
-
 var mouseMode = false;
 var keys = [];
-
 var defineAdjacentAreas = {
     mouseMoved: function () {
         if (mouseX < colorMap.width && mouseY < colorMap.height) {
             currClr = this.myGui.getCurrColor();
-
             if (((this.selected && highlightedAreaName.localeCompare(this.selected.name) != 0) || (this.selected === undefined)) && (highlightedAreaName in areas)) {
                 this.myGui.resetHighlight();
             }
-
             if (currClr in colorAreaNameMap) {
                 var currAreaName = colorAreaNameMap[currClr];
                 if ((this.selected === undefined) || (this.selected.name.localeCompare(currAreaName) != 0)) {
@@ -51,18 +44,17 @@ var defineAdjacentAreas = {
                     this.selected = areas[highlightedAreaName];
                     highlightedAreaName = '';
                     defineAdjacentAreas.updateGui();
-
-                } else {
+                }
+                else {
                     if (this.selected.adjacentAreaNames === undefined) {
                         this.selected.adjacentAreaNames = [];
                     }
-
                     var index = this.selected.adjacentAreaNames.indexOf(highlightedAreaName);
-
                     if (index != -1) {
                         this.selected.adjacentAreaNames.splice(index, 1);
                         defineAdjacentAreas.removeListItem(highlightedAreaName);
-                    } else {
+                    }
+                    else {
                         this.selected.adjacentAreaNames.push(highlightedAreaName);
                         defineAdjacentAreas.addListItem(highlightedAreaName);
                     }
@@ -71,32 +63,27 @@ var defineAdjacentAreas = {
         }
     }
     , updateGui: function () {
-
         if (defineAdjacentAreas.selectedAreaDiv) {
             defineAdjacentAreas.selectedAreaDiv.html('Selected area: ' + this.selected.name);
-        } else {
+        }
+        else {
             defineAdjacentAreas.selectedAreaDiv = createDiv('Selected area: ' + this.selected.name)
         }
-
         if (defineAdjacentAreas.adjacentList === undefined) {
             defineAdjacentAreas.adjacentList = createElement('ul');
-        } else {
-
+        }
+        else {
             var listElt = defineAdjacentAreas.adjacentList.elt;
-
             while (listElt.firstChild) {
                 listElt.removeChild(listElt.firstChild);
             }
         }
-
         if (this.selected.adjacentAreaNames) {
-
             for (i = 0; i < this.selected.adjacentAreaNames.length; i++) {
                 var listItem = createElement('li', this.selected.adjacentAreaNames[i]);
                 listItem.parent(defineAdjacentAreas.adjacentList);
             }
         }
-
     }
     , addListItem: function (areaName) {
         defineAdjacentAreas.adjacentList.child(createElement('li', areaName));
@@ -104,7 +91,6 @@ var defineAdjacentAreas = {
     , removeListItem: function (areaName) {
         for (i = 0; i < defineAdjacentAreas.adjacentList.elt.children.length; i++) {
             var currItem = defineAdjacentAreas.adjacentList.elt.children[i];
-
             if (currItem.innerHTML.localeCompare(areaName) == 0) {
                 defineAdjacentAreas.adjacentList.elt.removeChild(currItem);
             }
@@ -115,9 +101,7 @@ var defineAdjacentAreas = {
             this.myGui.resetHighlight(this.selected.name);
             delete this.selected;
             defineAdjacentAreas.selectedAreaDiv.html('');
-
             var listElt = defineAdjacentAreas.adjacentList.elt;
-
             while (listElt.firstChild) {
                 listElt.removeChild(listElt.firstChild);
             }
@@ -126,13 +110,11 @@ var defineAdjacentAreas = {
 }
 
 function Gui(game) {
-
     this.preload = function () {
         loadImage(GAME_MAP_PATH, function (image) {
             gameMap = image;
             gameMap.loadPixels();
         })
-
         loadImage(COLOR_MAP_PATH, function (image) {
             colorMap = image;
             colorMap.loadPixels();
@@ -142,39 +124,37 @@ function Gui(game) {
             colorAreaNameMap = colorAndAreas[1];
         })
     }
-
     this.guiInit = function () {
-        currTurnP = createP('Waiting For: ');
-        nextPhaseButton = createButton('Next Phase');
-        nextPhaseButton.hide();
-    }
-
-    // Control functions
-
-    // player - The player who's turn it is
-    //
-    // amount - The amount of unit he should be allowed to place(optional, 
-    //          if specified the amount will be limited, otherwise the player 
-    //          may choose how many units he would like to place)
-    // unclaimed - A boolean specifing whether or not the player should be forced to place units on unclaimed areas
-    //
-    // callback - A function that will be called when the player finished, recieved a parameter(amount of units placed)
+            currTurnP = createP('Waiting For: ');
+            nextPhaseButton = createButton('Next Phase');
+            nextPhaseButton.hide();
+        }
+        // Control functions
+        // player - The player who's turn it is
+        //
+        // amount - The amount of unit he should be allowed to place(optional, 
+        //          if specified the amount will be limited, otherwise the player 
+        //          may choose how many units he would like to place)
+        // unclaimed - A boolean specifing whether or not the player should be forced to place units on unclaimed areas
+        //
+        // callback - A function that will be called when the player finished, recieved a parameter(amount of units placed)
     this.nextGameState = function (player, callback, gameState) {
+        var btlDialog = new BattleDialogController({
+            units: 5
+        }, {});
+        btlDialog.show();
         this.gameState = gameState;
         this.callback = callback;
         this.setCurrPlayer(player);
         watingForGameLogic = false;
     }
-
     this.setCurrPlayer = function (player) {
-        currPlayer = player;
-        this.onPlayerChange();
-    }
-
-    // Events
+            currPlayer = player;
+            this.onPlayerChange();
+        }
+        // Events
     this.onPlayerChange = function () {
         this.updatePlayerStatus();
-
         switch (this.gameState) {
         case GAME_STATES.DEPLOY_CLAIMED:
             {
@@ -189,126 +169,99 @@ function Gui(game) {
             {
                 nextPhaseButton.hide();
                 break;
-
             }
         }
     }
-
     this.showNextButton = function () {
         this.removePreviousEventListeners();
         nextPhaseButton.mousePressed(this.callback);
         nextPhaseButton.show();
     }
-
     this.removePreviousEventListeners = function () {
         for (var ev in nextPhaseButton._events) {
             nextPhaseButton.elt.removeEventListener(ev, nextPhaseButton._events[ev]);
         }
-
         // Clean events map
         nextPhaseButton._events = {};
-
     }
-
     this.updatePlayerStatus = function () {
-        // Update curr player paragraph
-        currTurnP.html('Wating for: ' + currPlayer.name + '<br> Units Left: ' + currPlayer.unitsToPlace);
-    }
-
-
-    // Drawing
+            // Update curr player paragraph
+            currTurnP.html('Wating for: ' + currPlayer.name + '<br> Units Left: ' + currPlayer.unitsToPlace);
+        }
+        // Drawing
     this.draw = function () {
         background(255);
         image(gameMap, 0, 0);
         this.drawAreaCenters();
     }
-
     this.highlightArea = function (area) {
         // Set highlighted area name
         highlightedAreaName = area.name;
-
         // highlight all pixels of the area
         for (i = 0; i < area.pixPos.length; i++) {
             this.highlightPixel(area.pixPos[i].x, area.pixPos[i].y);
         }
-
         gameMap.updatePixels();
     }
-
     this.resetHighlight = function (areaName) {
-
         var areaPix;
-
         if (areaName) {
             areaPix = areas[areaName].pixPos;
-        } else {
+        }
+        else {
             areaPix = areas[highlightedAreaName].pixPos;
-
             highlightedAreaName = '';
         }
-
         for (i = 0; i < areaPix.length; i++) {
             this.setPixColor(areaPix[i].x, areaPix[i].y, areaPix[i].rgb);
         }
-
         gameMap.updatePixels();
     }
-
     this.highlightPixel = function (x, y) {
-
         var pixIndex = 4 * (x + y * gameMap.width);
-
         // For starters make the pixel greener
         var r = gameMap.pixels[pixIndex];
         var g = gameMap.pixels[pixIndex + 1];
         var b = gameMap.pixels[pixIndex + 2];
-
         if (r != 0 && g != 0 && b != 0) {
             gameMap.pixels[pixIndex] = r * 0.5;
             gameMap.pixels[pixIndex + 1] = (g * 0.5 + 100) % 256;
             gameMap.pixels[pixIndex + 2] = b * 0.5;
         }
     }
-
     this.setPixColor = function (x, y, rgb) {
         var pixIndex = 4 * (x + y * gameMap.width);
         gameMap.pixels[pixIndex] = rgb[0];
         gameMap.pixels[pixIndex + 1] = rgb[1];
         gameMap.pixels[pixIndex + 2] = rgb[2];
     }
-
     this.drawAreaCenters = function () {
-        for (var area in areas) {
-            if (areas.hasOwnProperty(area)) {
-                if ('units' in areas[area]) {
-
-                    var unitsStr = areas[area].units.toString();
-
-                    push();
-                    colorMode(HSB);
-                    fill(areas[area].owner.color);
-                    noStroke();
-                    ellipse(areas[area].center.x, areas[area].center.y, centerSize);
-                    rectMode(CENTER);
-                    fill(255);
-                    text(unitsStr, areas[area].center.x, areas[area].center.y, textWidth(unitsStr), textSize());
-                    pop();
+            for (var area in areas) {
+                if (areas.hasOwnProperty(area)) {
+                    if ('units' in areas[area]) {
+                        var unitsStr = areas[area].units.toString();
+                        push();
+                        colorMode(HSB);
+                        fill(areas[area].owner.color);
+                        noStroke();
+                        ellipse(areas[area].center.x, areas[area].center.y, centerSize);
+                        rectMode(CENTER);
+                        fill(255);
+                        text(unitsStr, areas[area].center.x, areas[area].center.y, textWidth(unitsStr), textSize());
+                        pop();
+                    }
                 }
             }
         }
-    }
-
-
-    /*
-        Loops through the adjacent areas of currArea and executes isAreaAdjacentToSourceOrFof on them to determine
-        if a certian condition is met
-        
-    */
+        /*
+            Loops through the adjacent areas of currArea and executes isAreaAdjacentToSourceOrFof on them to determine
+            if a certian condition is met
+            
+        */
     this.areaAdjacencyFunc = function (currArea, opts) {
         if (currArea.name in currPlayer.areas && currArea.units > 1) {
             for (i = 0; i < currArea.adjacentAreaNames.length; i++) {
                 var adjacentAreaName = currArea.adjacentAreaNames[i];
-
                 // Only if func returns true we want to return
                 // otherwise we want the loop to continue
                 if (this.isAreaAdjacentToSourceOrFof(adjacentAreaName, opts)) {
@@ -316,45 +269,33 @@ function Gui(game) {
                 }
             }
         }
-
         return false;
-
     }
-
     this.isAreaAdjacentToSourceOrFof = function (adjacentAreaName, opts) {
-        var ownedByPlayer = (adjacentAreaName in currPlayer.areas);
-        return (((opts.adjacentToFoe && !ownedByPlayer) || (!opts.adjacentToFoe && ownedByPlayer)));
-    }
-
-    // IO
+            var ownedByPlayer = (adjacentAreaName in currPlayer.areas);
+            return (((opts.adjacentToFoe && !ownedByPlayer) || (!opts.adjacentToFoe && ownedByPlayer)));
+        }
+        // IO
     this.mouseMoved = function () {
         if (!watingForGameLogic) {
             if (mouseX < colorMap.width && mouseY < colorMap.height) {
-
                 currentColor = this.getCurrColor();
-
                 if (highlightedAreaName.localeCompare('') != 0) {
                     this.resetHighlight();
                 }
-
                 if (currentColor in colorAreaNameMap) {
-
                     var currAreaName = colorAreaNameMap[currentColor];
-
                     var highlight = false;
-
                     switch (this.gameState) {
                     case GAME_STATES.DEPLOY_UNCLAIMED:
                         {
                             highlight = !('owner' in areas[currAreaName]);
-
                             break;
                         }
                     case GAME_STATES.DEPLOY_CLAIMED:
                     case GAME_STATES.REINFORCE:
                         {
                             highlight = (areas[currAreaName].owner.name.localeCompare(currPlayer.name) == 0) && (currPlayer.unitsToPlace > 0);
-
                             break;
                         }
                     case GAME_STATES.BTTL:
@@ -362,14 +303,13 @@ function Gui(game) {
                             if (this.source) {
                                 var adjacentToSource = this.source.adjacentAreaNames.indexOf(currAreaName) != -1;
                                 var ownedByPlayer = currAreaName in currPlayer.areas;
-
                                 highlight = adjacentToSource && !ownedByPlayer;
-                            } else {
+                            }
+                            else {
                                 highlight = this.areaAdjacencyFunc(areas[currAreaName], {
                                     adjacentToFoe: true
                                 }) && (areas[currAreaName].units > 1);
                             }
-
                             break;
                         }
                     case GAME_STATES.FORT:
@@ -377,33 +317,27 @@ function Gui(game) {
                             if (this.source) {
                                 var adjacentToSource = this.source.adjacentAreaNames.indexOf(currAreaName) != 0;
                                 var ownedByPlayer = currAreaName in currPlayer.areas;
-
                                 highlight = adjacentToSource && ownedByPlayer;
-                            } else {
+                            }
+                            else {
                                 highlight = this.areaAdjacencyFunc(areas[currAreaName], {
                                     adjacentToFoe: false
                                 }) && (areas[currAreaName].units > 1);
-
                             }
-
                             break;
                         }
                     }
-
                     if (highlight) {
                         this.highlightArea(areas[currAreaName]);
                     }
                 }
-
             }
         }
     }
-
     this.mousePressed = function mousePressed() {
         if (!watingForGameLogic) {
             if (mouseButton == LEFT && this.mouseWithinCanvas()) {
                 if (highlightedAreaName in areas) {
-
                     switch (this.gameState) {
                     case GAME_STATES.DEPLOY_UNCLAIMED:
                         {
@@ -413,10 +347,8 @@ function Gui(game) {
                                 currPlayer.areas[highlightedAreaName] = areas[highlightedAreaName];
                                 currPlayer.unitsToPlace--;
                                 this.resetGui();
-                                this.callback();
+                                //this.callback();
                             }
-
-
                             break;
                         }
                     case GAME_STATES.DEPLOY_CLAIMED:
@@ -427,12 +359,10 @@ function Gui(game) {
                         {
                             areas[highlightedAreaName].units++;
                             currPlayer.unitsToPlace--;
-
                             if (currPlayer.unitsToPlace == 0) {
                                 this.resetGui();
                                 this.callback();
                             }
-
                             break;
                         }
                     case GAME_STATES.BTTL:
@@ -440,20 +370,17 @@ function Gui(game) {
                             if (this.source) {
                                 var btlDialog = new BattleDialogController(this.source, areas[highlightedAreaName]);
                                 btlDialog.show();
-                            } else {
+                            }
+                            else {
                                 this.source = areas[highlightedAreaName];
                             }
-
                         }
-
                     }
-
                     this.updatePlayerStatus();
                 }
             }
         }
     }
-
     this.mouseDragged = function () {
         /* if (this.mouseWithinCanvas() && (highlightedAreaName in areas)) {
              areas[highlightedAreaName].center = {
@@ -462,9 +389,7 @@ function Gui(game) {
              };
          }*/
     }
-
     this.handleMouseWheel = function (e) {}
-
     this.keyPressed = function () {
         if (!watingForGameLogic) {
             // Safegaurd
@@ -473,7 +398,6 @@ function Gui(game) {
             }
         }
     }
-
     this.keyReleased = function () {
         if (!watingForGameLogic) {
             if (keys.indexOf(keyCode) != -1) {
@@ -481,42 +405,32 @@ function Gui(game) {
             }
         }
     }
-
     this.keyComboPressed = function (keyCombo) {
-
-        for (i = 0; i < keyCombo.length; i++) {
-            if (keys.indexOf(keyCombo[i]) == -1) {
-                return false;
+            for (i = 0; i < keyCombo.length; i++) {
+                if (keys.indexOf(keyCombo[i]) == -1) {
+                    return false;
+                }
             }
+            // Guarding against states when key combo is pressed with additional characters
+            return true && (keys.length == keyCombo.length);
         }
-
-        // Guarding against states when key combo is pressed with additional characters
-        return true && (keys.length == keyCombo.length);
-    }
-
-    // Helper Functions
-
+        // Helper Functions
     this.mouseWithinCanvas = function () {
         return (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height);
     }
-
     this.getCurrColor = function () {
         var r = colorMap.pixels[4 * (mouseX + mouseY * colorMap.width)];
         var g = colorMap.pixels[4 * (mouseX + mouseY * colorMap.width) + 1];
         var b = colorMap.pixels[4 * (mouseX + mouseY * colorMap.width) + 2];
         var a = colorMap.pixels[4 * (mouseX + mouseY * colorMap.width) + 3];
-
         return (hex(r, 2) + hex(g, 2) + hex(b, 2));
     }
-
     this.resetGui = function () {
         if (highlightedAreaName in areas) {
             this.resetHighlight();
         }
-
         // Wait until callback calls gui again
         nextPhaseButton.elt.disabled = true;
         watingForGameLogic = true;
-
     }
 }
